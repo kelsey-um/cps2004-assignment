0af00f14bf691d2b56f698d1b25f85f7a83fdfd6
kelsey-um <kelsey.bonnici.21@um.edu.mt>
2022-11-02 20:26:38 +0100

    added displayGrid(), showCell(), checkConditions() and winning conditions

--- task 2/header.hpp
+++ task 2/header.hpp
@@ -5,17 +5,19 @@ using namespace std;
 class Cell{
     public:
         string value = "00"; //holds the value of the cell
-        bool isHidden = false; //USE TO DEBUG
+        bool isHidden = true; //USE TO DEBUG - DEFAULT = TRUE
 };
 
 class Minesweeper{
 
     //according to assignment
     static const int size = 16;
-    static const int noMines = 40;
-
+    
     public:
         Cell grid[size][size];       
+        int totalCells = size*size;
+        int cellsOpened = 0;
+        const int noMines = 40; //DEFAULT = 40
 
     void displayGrid(){
 
@@ -56,6 +58,7 @@ class Minesweeper{
             cout << endl;
 
         }
+        cout << endl;
     }
 
     bool checkMine(int y, int x){
@@ -96,63 +99,167 @@ class Minesweeper{
                 // to increment adjacent mines
 
                 //x-1 y-1
-                if(!((randY-1 < 0) || (randX-1 < 0)) && !(checkMine(randY-1, randX-1))){
-                
+                if(!((randY-1 < 0) || (randX-1 < 0)) && !(checkMine(randY-1, randX-1))){                
                     incrementValue(randY-1, randX-1);
-
                 }
 
                 //x y-1
                 if(!(randX-1 < 0) && !(checkMine(randY, randX-1))){
-
                     incrementValue(randY, randX-1);
-
                 }
 
                 //x+1 y-1
                 if(!((randY+1 > 15) || (randX-1 < 0)) && !(checkMine(randY+1, randX-1))){
-
                     incrementValue(randY+1, randX-1);
-
                 }
 
                 //x-1 y
-                if(!(randY-1 < 0) && !(checkMine(randY-1, randX))){
-                    
+                if(!(randY-1 < 0) && !(checkMine(randY-1, randX))){                   
                     incrementValue(randY-1, randX);
-
                 }
 
                 //x+1 y
                 if(!(randY+1 > 15) && !(checkMine(randY+1, randX))){
-
                     incrementValue(randY+1, randX);
-
                 }
 
                 //x-1 y+1 
                 if(!((randY-1 < 0) || (randX+1 > 15)) && !(checkMine(randY-1, randX+1))){
-
                     incrementValue(randY-1, randX+1);
-
                 }
 
                 //x y+1 
-                if(!(randX+1 > 15) && !(checkMine(randY, randX+1))){
-                
+                if(!(randX+1 > 15) && !(checkMine(randY, randX+1))){               
                     incrementValue(randY, randX+1);
-
                 }
 
                 //x+1 y+1
                 if(!((randY+1 > 15) || (randX+1 > 15)) && !(checkMine(randY+1, randX+1))){
-
                     incrementValue(randY+1, randX+1);
-
                 }
 
             }
 
         }
+
+        displayGrid();
     }
+
+    void userInput(){
+
+        int userX, userY;
+
+        //check if cell is hidden
+        while(true){
+            cout << "Enter x coordinate between 0 and 15" << endl;
+            cin >> userX;
+
+            //input validation
+            while(cin.fail() || ((userX < 0 || userX > 15))){ 
+            
+            cout << "Only integers between 0 and 15 are accepted. Please try again." << endl;
+            cin.clear();
+            cin.ignore(256,'\n');
+            cin >> userX;
+
+        }
+       
+            cout << "Enter y coordinate" << endl;
+            cin >> userY;
+
+            //input validation
+            while(cin.fail() || ((userY < 0 || userY > 15))){ 
+            
+            cout << "Only integers between 0 and 15 are accepted. Please try again." << endl;
+            cin.clear();
+            cin.ignore(256,'\n');
+            cin >> userY;
+
+        }
+
+        
+            if(grid[userY][userX].isHidden == false){
+                cout << "This cell is already showing. Please try again" << endl;
+            } else {
+                break;
+            }
+        }
+
+        checkConditions(userX, userY);
+
+    }
+
+    void showCell(int y, int x){
+
+        if(grid[y][x].isHidden == true){    //so theres no duplicate  
+            grid[y][x].isHidden = false;
+            cellsOpened++;
+        }
+    
+    }
+    
+    void checkConditions(int x, int y){
+        
+        if(grid[y][x].value == "XX"){ //mine is hit -- tested
+
+            //set every cell to show
+           for(int i = 0 ; i < size ; i++){
+                for(int j = 0 ; j < size ; j++){
+                    grid[i][j].isHidden = false;
+                }
+           }
+
+           displayGrid();
+
+           cout << "You have hit a mine. Game Over!";
+
+        } else if(grid[y][x].value == "00"){ //00 is hit and 8 adjacent open
+            
+            showCell(y,x);
+
+            if(!((y-1 < 0) || (x-1 <0)) && !(checkMine(y-1, x-1))){
+                showCell(y-1, x-1);
+            }
+
+            if(!(x-1 < 0) && !(checkMine(y,x-1))){
+                showCell(y, x-1);
+            }
+
+            if(!((y+1 > 15) || (x-1 < 0)) && !(checkMine(y+1, x-1))){
+                showCell(y+1, x-1);
+            }
+
+            if(!(y-1 < 0) && !(checkMine(y-1, x))){
+                showCell(y-1, x);
+            }
+
+            if(!(y+1 > 15) && !(checkMine(y+1, x))){
+                showCell(y+1, x);
+            }
+
+            if(!((y-1 < 0) || (x+1 > 15)) && !(checkMine(y-1, x+1))){
+                showCell(y-1, x+1);
+            }
+            
+            if(!(x+1 > 15) && !(checkMine(y, x+1))){
+                showCell(y, x+1);
+            }
+
+            if(!((y+1 > 15) || (x+1 > 15)) && !(checkMine(y+1, x+1))){
+                showCell(y+1, x+1);
+            }
+            
+            displayGrid();
+
+
+
+        } else { //cell has numerical value
+            showCell(y,x);
+            displayGrid();
+        }
+
+        
+
+    }
+
 };
--- task 2/minesweeper.cpp
+++ task 2/minesweeper.cpp
@@ -2,11 +2,22 @@
 
 int main(){
 
+    bool win = false;
+
     Minesweeper game;
 
     game.generateGridValues();
 
-    game.displayGrid();
+    while(game.cellsOpened < (game.totalCells-game.noMines)){
+        game.userInput();
+        win = true;
+    }
+
+    if (win = true){
+        cout << "Good job! You've won the game.";
+    }
+
+
 
     return 0;
 }
